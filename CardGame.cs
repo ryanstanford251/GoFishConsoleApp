0c32a5743d44e639a7f6cec42126dace5076ccd0
Ryan <ryanstanford251@gmail.com>
2021-01-31 20:23:02 -0500

    updated namespace of CardGame class

--- CardGame.cs
+++ CardGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-namespace GoFishConsoleApp
+namespace Cards
 {
     static class CardGame
     {
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using static GoFishConsoleApp.CardGame;
+using static Cards.CardGame;
 
 namespace GoFishConsoleApp
 {
